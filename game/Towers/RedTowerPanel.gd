f5a0c784c0048e4650d807d9637dfc3ecbdfcd83
bouj2663 <jonathan.bouthiette@usherbrooke.ca>
2024-02-18 13:15:25 -0500

    Added Upgrades and Fixed UI
    Based on these video tutorials:
    https://youtu.be/hWNDX5C80As?si=kXOXQ1acJlWeZahm
    &
    https://youtu.be/xkI6Q3zw8Ps?si=OAWIsd7pp-5KVNym

--- game/Towers/RedTowerPanel.gd
+++ game/Towers/RedTowerPanel.gd
@@ -15,25 +15,34 @@ func _on_gui_input(event):
 		tempTower.scale = Vector2(0.32, 0.32)
 		
 		
-	elif event is InputEventMouseButton and event.button_mask == 1:
+	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#print("Left button up")
 		if get_child_count() > 1:	
+			
 			get_child(1).global_position = event.global_position
 		
+			var mapPath = get_tree().get_root().get_node("Main/TileMap")
+			var tile = mapPath.local_to_map(get_global_mouse_position())
+			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+			
+			if (currTile == Vector2i(4,5)):
+				get_child(1).get_node("Area").modulate = Color(0,255,0, 0.3)
+			else:
+				get_child(1).get_node("Area").modulate = Color(255,255,255, 0.3)
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#print("Left button up")
-		if event.global_position.x >= 2944:
+		if event.global_position.x >= 2304: #2944
 			if get_child_count() > 1:	
 				get_child(1).queue_free()
 		else:
 			if get_child_count() > 1:	
 				get_child(1).queue_free()
-			var path = get_tree().get_root().get_node("Main/Towers")
-			
-			path.add_child(tempTower)
-			tempTower.global_position = event.global_position
-			tempTower.get_node("Area").hide()
-		
+			if currTile == Vector2i(4,5):
+				var path = get_tree().get_root().get_node("Main/Towers")
+				
+				path.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("Area").hide()
 	else:
 		if get_child_count() > 1:
 			get_child(1).queue_free()
