4b23eb0cd855e3e671ccb961a10b150869c36b15
bouj2663 <jonathan.bouthiette@usherbrooke.ca>
2024-02-22 13:47:10 -0500

    Fixed Health Bug and multiple spawns

--- game/Mobs/Soldier A.gd
+++ game/Mobs/Soldier A.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-@export var speed = 250
+@export var speed = 1000
 var Health = 10
 
-func _process(delta):
+func _physics_process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	
-	if get_parent().get_progress_ratio() == 1:
-		Game.health -= 1
+	if get_parent().get_progress_ratio() >= 0.999:
+		Game.Health -= 1
 		death()
 		
 	if Health <= 0:
@@ -16,4 +16,5 @@ func _process(delta):
 		death()
 
 func death():
+	print(get_parent().get_progress_ratio())
 	get_parent().get_parent().queue_free()
--- game/Towers/RedBullet.gd
+++ game/Towers/RedBullet.gd
@@ -11,12 +11,10 @@ func _physics_process(delta):
 	for i in pathSpawnerNode.get_child_count():
 		if pathSpawnerNode.get_child(i).name == pathName:
 			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
-	
-	velocity = global_position.direction_to(target) *Speed
-	
-	look_at(target)
-	
-	move_and_slide()
+			# Pour solve le bug de vélocité, j'ai tabulé les trois lignes suivants dans le if. New lesser bug: Certains missiles ne bougent pas.
+			velocity = global_position.direction_to(target) *Speed
+			look_at(target)
+			move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
