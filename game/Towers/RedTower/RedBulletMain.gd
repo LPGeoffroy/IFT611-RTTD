450a2066f5857861d7fb56fbdc23dcf628dd11ea
cimo2801 <olivier.cimon@usherbrooke.ca>
2024-04-12 20:37:43 -0400

    RedBulletTower fonctionnel sur toutes les path

--- game/Towers/RedTower/RedBulletMain.gd
+++ game/Towers/RedTower/RedBulletMain.gd
@@ -1,27 +1,35 @@
 extends CharacterBody2D
 
 var target
+var targetPosition
 var Speed = 1000
 var pathName = ""
 var bulletDamage
 var timer := Timer.new()
 
 func _physics_process(_delta):
+	var pathSpawnerNode
+	if not "Shortcut" in pathName:
+		pathSpawnerNode = get_tree().get_root().get_node(str(Game.currentMap) + "/PathSpawner")
+	else:
+		pathSpawnerNode = get_tree().get_root().get_node(str(Game.currentMap) + "/ShortcutPathSpawner")
+		
+	if not is_instance_valid(target):	# TODO : Trouver un autre target au lieu de détruire le missile
+		print("Target dead")
+		queue_free()
+		return
+	targetPosition = target.global_position
 	
-	var pathSpawnerNode = get_tree().get_root().get_node(str(Game.currentMap) + "/PathSpawner")
-	for i in pathSpawnerNode.get_child_count():
-		if pathSpawnerNode.get_child(i).name == pathName:
-			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
-			# Pour solve le bug de vélocité, j'ai tabulé les trois lignes suivants dans le if. New lesser bug: Certains missiles ne bougent pas.
-			velocity = global_position.direction_to(target) *Speed
-			look_at(target)
-			move_and_slide()
-			# Timer pour que les missiles disparraissent apres x secondes
-			timer.wait_time = 1.5 # 1.5 seconds
-			timer.one_shot = true # don't loop, run once
-			timer.autostart = true # start timer when added to a scene
-			timer.timeout.connect(_on_timer_timeout)
-			add_child(timer)
+	# Pour solve le bug de vélocité, j'ai tabulé les trois lignes suivants dans le if. New lesser bug: Certains missiles ne bougent pas.
+	velocity = global_position.direction_to(targetPosition) * Speed
+	look_at(targetPosition)
+	move_and_slide()
+	# Timer pour que les missiles disparraissent apres x secondes
+	timer.wait_time = 1.5 # 1.5 seconds
+	timer.one_shot = true # don't loop, run once
+	timer.autostart = true # start timer when added to a scene
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
 
 func _on_timer_timeout() -> void:
 	queue_free()
--- game/Towers/RedTower/RedBulletTower.gd
+++ game/Towers/RedTower/RedBulletTower.gd
@@ -1,72 +1,74 @@
 extends StaticBody2D
 
-
+@onready var timer = get_node("Upgrade/ProgressBar/Timer")
 var Bullet = preload("res://Towers/RedTower/RedBulletMain.tscn")
-var AltBullet = preload("res://Towers/RedTower/RedBulletShortcut.tscn")
 var WhichBullet = 0
 var bulletDamage = 5
-var pathName
-var currTargets = []
-var curr
-#@onready var mainPathSpawnerNode = get_tree().get_root().get_node(str(Game.currentMap) + "/PathSpawner")
-#@onready var shortcutPathSpawnerNode = get_tree().get_root().get_node(str(Game.currentMap) + "/ShortcutPathSpawner")
+var pathName			# Le pathname du targer courant
+var currTargets = []	# Les mobs actuellement dans le range
+var curr				# Le target courant
 var reload = 0
 var range = 400
 
-@onready var timer = get_node("Upgrade/ProgressBar/Timer")
-var startShooting = false
 
 func _process(_delta):
 	get_node("Upgrade/ProgressBar").global_position = self.position + Vector2(-64, -48)
+	if not is_instance_valid(curr):
+		find_target()
 	if is_instance_valid(curr):
 		self.look_at(curr.global_position)
-		if timer.is_stopped():
+		if timer.is_stopped() && !currTargets.is_empty():
 			Shoot()
 			timer.start()
-	else:
-		for i in get_node("BulletContainer").get_child_count():
-			get_node("BulletContainer").get_child(i).queue_free()
+		elif timer.is_paused() && !currTargets.is_empty():
+			Shoot()
+			timer.set_paused(false)
 	update_powers()
+
 func Shoot():
 	var tempBullet = Bullet.instantiate()
-	if WhichBullet == 1:
-		tempBullet = AltBullet.instantiate()
 	tempBullet.pathName = pathName
 	tempBullet.bulletDamage = bulletDamage
 	get_node("BulletContainer").add_child(tempBullet)
 	tempBullet.global_position = $Aim.global_position
-		
+	tempBullet.target = curr
+
+
 func _on_tower_body_entered(body):
-	#Ne sélectionne comme target que des cibles sur son path
 	if "Soldier" in body.name:
-		var tempArray = []
-		currTargets = get_node("Tower").get_overlapping_bodies()
+		currTargets.append(body)
+		
+		for mob in currTargets: 
+			if not is_instance_valid(mob):
+				continue
+			if get_node("Tower").get_overlapping_bodies().find(mob):
+				if curr == null:
+					curr = mob
+				else:
+					if mob.get_parent().get_progress() > curr.get_parent().get_progress():
+						# Attaque le mob le plus loin dans le path
+						curr = mob	
+		pathName = curr.get_node("../").get_parent().name
 
-		for i in currTargets:
-			if WhichBullet == 0:
-				if "Soldier" in i.name:
-					#var iParent = i.Path
-					if not "Shortcut" in i.Path.get_name(2):
-						tempArray.append(i)
-			else:
-				if "Soldier" in i.name:
-					if "Shortcut" in i.Path.get_name(2):
-						tempArray.append(i)
 
-		var currTarget = null
-		
-		for i in tempArray:
-			if currTarget == null:
-				currTarget = i.get_node("../")
-			else:
-				if i.get_parent().get_progress() > currTarget.get_progress():
-					currTarget = i.get_node("../")
-			curr = currTarget		
-			pathName = currTarget.get_parent().name
-		
-		
-func _on_tower_body_exited(_body):
-	currTargets = get_node("Tower").get_overlapping_bodies()
+func _on_tower_body_exited(body):
+	if "Soldier" in body.name:
+		var index = currTargets.find(body)
+		currTargets.remove_at(index)
+		curr = null
+
+
+func find_target():
+	var tmpTarget = null
+	for mob in currTargets:
+		if is_instance_valid(mob):
+			if tmpTarget == null:
+				tmpTarget = mob
+			elif mob.get_parent().get_progress() > tmpTarget.get_parent().get_progress():
+				tmpTarget = mob
+	if tmpTarget != null:
+		curr = tmpTarget
+		pathName = curr.get_node("../").get_parent().name
 
 
 func _on_input_event(_viewport, event, _shape_idx):
@@ -83,26 +85,32 @@ func _on_input_event(_viewport, event, _shape_idx):
 
 
 func _on_timer_timeout():
-	Shoot()
-	
+	if is_instance_valid(curr):
+		Shoot()
+	else:
+		timer.set_paused(true)
+
 
 func _on_range_pressed():
 	if range <= 750:
 		if Game.gold >= 10:
 			Game.gold -= 10
 			range += 50
-	
+
+
 func _on_attack_speed_pressed():
 	if reload <= 2:
 		if Game.gold >= 10:
 			Game.gold -= 10
 			reload += 0.1
 	timer.wait_time = 3 - reload
-	
+
+
 func _on_power_pressed():
 	if Game.gold >= 10:
 		Game.gold -= 10
 		bulletDamage += 1
+
 
 func update_powers():
 	get_node("Upgrade/Upgrade/HBoxContainer/Range/Label").text = str(range)
